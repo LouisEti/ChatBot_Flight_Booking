e2084891861fce4832fc95f9fe6e8a030461ba27
LouisEti <111647076+LouisEti@users.noreply.github.com>
2023-01-20 16:55:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     app = init_func(None) #None for main
     try:
         # Run app in production
-#         web.run_app(app, host='0.0.0.0', port=CONFIG.PORT)
-        web.run_app(app, host='localhost', port=CONFIG.PORT)
+        web.run_app(app, host='0.0.0.0', port=CONFIG.PORT)
+#         web.run_app(app, host='localhost', port=CONFIG.PORT)
     except Exception as error:
         raise error
