88d7ccaf280c20b508babf152ca3360f1a40caa2
LouisEti <111647076+LouisEti@users.noreply.github.com>
2023-01-20 05:31:31 +0100

    Update bot_config.py
    Change MicrosftApp keys

--- bot_config.py
+++ bot_config.py
@@ -13,8 +13,8 @@ class DefaultConfig:
     PORT = 3978
     # APP_ID = os.environ.get("MicrosoftAppId", "") 
     # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    APP_ID = os.environ.get("MicrosoftAppId", "") 
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "b0f22e08-bfcf-4ac3-b637-82efc5278d37") 
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "41fd1518-b497-4e9e-9406-95fde84ac7e2")
 
     ############## LUIS Service ###############
     LUIS_APP_ID = os.environ.get("LuisAppId", "109b72ce-ba2e-48f4-b3e6-06889b9320a4") #109b72ce-ba2e-48f4-b3e6-06889b9320a4
